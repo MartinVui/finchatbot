17532c406e93d41d585f198c318d1398ac78ae8e
Martin <mar.vuillemard@gmail.com>
2016-09-23 16:10:19 +0200

    created a frame to implement on client websites

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,70 +8,16 @@ import { Messages } from '../api/messages.js';
 import Message from './Message.jsx';
 
 import ChatBox from './ChatBox.jsx';
-import Slides from './Slides.jsx';
 
 class App extends Component {
-
-
-	constructor(props) {
-    	super(props);
-    	this.state = {
-      		showChatBox: false,
-          mounted:false
-    	};
-    	this.onLogoClick = this.onLogoClick.bind(this);
-  //    Session.set('json', '');
-  	}
-
-  	
- 	onLogoClick() {
- 		if(this.state.showChatBox == false) {
-     		this.setState({
-     			showChatBox: true
-     		});
-
-    }
-      else if(this.state.showChatBox == true) {
-     		this.setState({
-     			showChatBox: false
-     		});
-      }
-  }
-
-  displayChatBox() {
-// Automatically open the chatbox after 10 sec
-
-    this.setState({
-      showChatBox:true
-    });
-  }
-	
-  componentWillMount() {
-    Session.set('showGif', false);
-    var TIMEOUT = setTimeout(this.displayChatBox.bind(this), 10000);
-  }
 
 	
   	render() {
 
     	return (
-
-
     		<div>
-        
-          <img src='images/sized_background.jpg' className='background2'/>
-          
-          <Slides imageSrc={Session.get('slide')}/>  		
-          
-          {/*<img src='images/allan_grey_opening_page.jpg' className='background' />*/}
-    	    <p><img src='images/logo.png' className='logo' onClick={this.onLogoClick}></img></p>
-    			
-    	    {this.state.showChatBox ?
-        		<ChatBox messages={this.props.messages}/>: null
-        	}
-
+        	<ChatBox messages={this.props.messages}/>       	
         </div>
-
     	);
   	}
 }
--- imports/ui/ChatBox.jsx
+++ imports/ui/ChatBox.jsx
@@ -18,7 +18,7 @@ export default class ChatBox extends Component {
 	// Render the whole chatbox when the user click on the chatbot logo
 	
 		return(
-			<div><div className="container">
+			<div className="container">
 	        	
 	        	<div className="conversation">
 	        		<MessageList messages={this.props.messages}/>
@@ -27,7 +27,7 @@ export default class ChatBox extends Component {
 	        	<MessageForm onMessageSubmit={this.handleMessageSubmit}/>
 	        	</footer>
      		</div>
-     		<p><img src='images/bulle_grise.png' className='bulle'></img></p></div>
+     		
 
 	    );
 	}
