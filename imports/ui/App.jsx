f9e0be4cc2957afe76e488877ffafc51d10bf115
Martin <mar.vuillemard@gmail.com>
2016-09-26 10:24:22 +0200

    modified the display of the iframe

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -12,54 +12,15 @@ import ChatBox from './ChatBox.jsx';
 class App extends Component {
 
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          showChatBox: false,
-      };
-      this.onLogoClick = this.onLogoClick.bind(this);
-  //    Session.set('json', '');
-    }
 
-    
-  onLogoClick() {
-    if(this.state.showChatBox == false) {
-        this.setState({
-          showChatBox: true
-        });
-
-    }
-      else if(this.state.showChatBox == true) {
-        this.setState({
-          showChatBox: false
-        });
-      }
-  }
-
-  displayChatBox() {
-// Automatically open the chatbox after 10 sec
-
-    this.setState({
-      showChatBox:true
-    });
-  }
-  
-  componentWillMount() {
-    Session.set('showGif', false);
-    var TIMEOUT = setTimeout(this.displayChatBox.bind(this), 10000);
-  }
-
-
-	
+ 
   	render() {
 
     	return (
-    		<div>
-        	<p><img src='images/logo.png' className='logo' onClick={this.onLogoClick}></img></p>
-          
-          {this.state.showChatBox ?
-            <ChatBox messages={this.props.messages}/>: null
-          }
+    		<div>          
+
+            <ChatBox messages={this.props.messages}/>
+
                 	
         </div>
     	);
