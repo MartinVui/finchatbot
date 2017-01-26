f586e940a2a02d351abcf43d80f28e276d50196b
etchemendymat <matteo.etchemendy@supelec.fr>
2017-01-26 10:02:38 +0200

    Add discussions

---
+++ imports/api/discussions.js
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+import { discussionSchema } from '../schemas/discussionSchema.js';
+
+export const Discussions = new Mongo.Collection("discussions");
+
+Meteor.methods({
+	'discussion.insert'(discussion){
+		check(discussion , discussionSchema);
+		Discussions.insert(discussion);
+	};
+	'discussion.remove'(discussionId){
+		check(discussionId , String);
+		Discussions.remove(discussionId);
+	};
+
+//Peut-être besoin d'ajouter une méthode d'empilage de questions
+
+});
---
+++ imports/schemas/discussionSchema.js
@@ -0,0 +1,8 @@
+export var AnswerSchema = new SimpleSchema({
+	user_content: {
+		type: String,
+	},
+	default_content: {
+		type: String,
+	}
+})
