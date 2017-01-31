4e14e6b3cd36418400d6adc721c7e3adca4eb4cd
fTrestour <ftrestour@gmail.com>
2017-01-31 15:39:41 +0100

    scanDiscussion almost done

--- imports/processes/scannDiscussion.js
+++ imports/processes/scannDiscussion.js
@@ -1,20 +1,23 @@
 export function scanDiscussion() {
 
- var scenario = Discussion.findOne({_id:discussion['idScenario']});
- var messages = [];
+  var scenario = Discussion.findOne({_id:discussion['idScenario']});
+  var messages = [];
 
- for (var answerId of discussion['answers']) {
+  for (var answerId of discussion['answers']) {
 
-   messages.push(Questions.findOne({_id:scenario['idQuestion']});
+    messages.push(Questions.findOne({_id:scenario['idQuestion']}));
+    messages.push(Answer.findOne({_id:answerId}));
 
-   var chosenAnswer = scenario['children'].filter(function ( obj ) {
-     return obj['idFormGenerator'] === Answers.findOne({_id:answerId})['idFormGenerator'];
-   })[0];
-   if (typeof chosenAnswer === "undefined") {
-     console.log("Problem in discussion");
-   } else {
-     scenario = chosenAnswer['']
-   }
+    var chosenAnswer = scenario['children'].filter(function ( obj ) {
+      return obj['idFormGenerator'] === Answers.findOne({_id:answerId})['idFormGenerator'];
+    })[0];
 
-   );
- }
+    if (typeof chosenAnswer === "undefined") {
+      console.log("Problem in discussion");
+    } else {
+      scenario = Scenarios.findOne({_id:chosenAnswer['idScenario']});
+    }
+
+    return messages;
+
+}
