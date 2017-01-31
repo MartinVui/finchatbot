517a5a3f0bc7cd81938ace2a1a7b3f08f784c9ae
etchemendymat <matteo.etchemendy@supelec.fr>
2017-01-31 15:46:35 +0200

    Fix

--- imports/processes/scenarioPicker.js
+++ imports/processes/scenarioPicker.js
@@ -2,7 +2,7 @@ import { Scenarios } from './api/scenarios.js';
 import { Random } from 'meteor/random';
 
 export function scenarioPicker(){
-	const InitScenarios = Scenarios.find({'initiate' : true });
+	const InitScenarios = Scenarios.find({'initiate' : true }).fetch();
 	return Random.choice(InitScenarios);
 };
 
