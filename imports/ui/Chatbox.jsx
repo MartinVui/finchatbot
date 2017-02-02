2660d7ff14b640d2b8501581e9693644a6520785
fTrestour <ftrestour@gmail.com>
2017-02-02 14:08:25 +0100

    minor changes

--- imports/ui/Chatbox.jsx
+++ imports/ui/Chatbox.jsx
@@ -66,7 +66,7 @@ export default class ChatBox extends Component {
 
             // Add discussion id to the session
             Session.set('SessionId' , discussionId);
-            
+
 
 
             // Return scenario Id
