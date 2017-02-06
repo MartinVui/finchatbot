aa285a92628db9e7dd65bd6a52c2d3c8c2971c97
fTrestour <ftrestour@gmail.com>
2017-02-06 15:36:58 +0100

    hotfix

--- imports/api/formgenerators.js
+++ imports/api/formgenerators.js
@@ -7,23 +7,13 @@ import { formGeneratorSchema } from './schemas/formGeneratorSchema.js';
 export const FormGenerators = new Mongo.Collection('formGenerators');
 
 Meteor.methods({
-'formGenerators.insert' (formGenerator) {
-    check(formGenerator, FormGeneratorSchema);
-    var newFormGenerator = FormGenerators.insert(formGenerator);
-    // console.log(newFormGenerator);
-    return formGenerator;
-},
-'formGenerators.remove' (formGeneratorId) {
-    FormGenerators.remove(formGeneratorId);
-}
-})
-orId) {
-    FormGenerators.remove(formGeneratorId);
-}
-})
-ormGeneratorId);
-}
-})
-ormGeneratorId);
-}
+    'formGenerators.insert' (formGenerator) {
+        check(formGenerator, FormGeneratorSchema);
+        var newFormGenerator = FormGenerators.insert(formGenerator);
+        // console.log(newFormGenerator);
+        return formGenerator;
+    },
+    'formGenerators.remove' (formGeneratorId) {
+        FormGenerators.remove(formGeneratorId);
+    }
 })
