ae39cb40be3b57005995912de5cdeb68c72501b1
fTrestour <ftrestour@gmail.com>
2017-02-06 15:20:53 +0100

    Indentation cleaning

--- imports/api/discussions.js
+++ imports/api/discussions.js
@@ -1,21 +1,33 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
-import { DiscussionSchema } from './schemas/discussionSchema.js';
+import {
+    Meteor
+} from 'meteor/meteor';
+import {
+    Mongo
+} from 'meteor/mongo';
+import {
+    check
+} from 'meteor/check';
+import {
+    DiscussionSchema
+} from './schemas/discussionSchema.js';
 
 export const Discussions = new Mongo.Collection("discussions");
 
 Meteor.methods({
-	'discussion.insert'(discussion){
-		check(discussion , DiscussionSchema);
-     	var newDiscussion = Discussions.insert(discussion);
-     	return newDiscussion;
-	},
-	'discussion.remove'(discussionId){
-		check(discussionId , String);
-		Discussions.remove(discussionId);
-	},
-
-//Peut-être besoin d'ajouter une méthode d'empilage de questions
+    'discussion.insert' (discussion) {
+        check(discussion, DiscussionSchema);
+        var newDiscussion = Discussions.insert(discussion);
+        return newDiscussion;
+    },
+    'discussion.remove' (discussionId) {
+        check(discussionId, String);
+        Discussions.remove(discussionId);
+    },
+    'discussion.update' (discussionId, update) {
+        check(discussionId, String);
+        Discussions.update(discussionId, {
+            $set: update
+        });
+    }
 
 });
