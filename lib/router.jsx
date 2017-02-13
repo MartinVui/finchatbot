c8d335aa5afc3ff0c0ca265b8ee7ecb810f1a406
etchemendymat <matteo.etchemendy@supelec.fr>
2017-02-13 17:30:32 +0200

    fix

--- lib/router.jsx
+++ lib/router.jsx
@@ -21,14 +21,17 @@ Router.route( "/messenger", { where: "server" } )
 	    	console.error("Failed validation. Make sure the validation tokens match.");
 	    	res.sendStatus(403);          
 		}  
-	});
+	})
+
   .post( function() {
     // If a POST request is made, create the user's profile.
   })
+
   .put( function() {
     // If a PUT request is made, either update the user's profile or
    // create it if it doesn't already exist.
   })
+
   .delete( function() {
    // If a DELETE request is made, delete the user's profile.
   });
