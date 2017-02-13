6086659492acce56b64297c98f658e6d15405816
fTrestour <ftrestour@gmail.com>
2017-02-13 15:15:32 +0100

    set up messenger endpoint

--- lib/router.jsx
+++ lib/router.jsx
@@ -11,3 +11,18 @@ Router.route('/', () => {
   );
   render(page, document.getElementById( 'render-target' ));
 });
+
+  .get( function() {
+    this.response.setHeader( 'access-control-allow-origin', '*' );
+    this.response.statusCode = 200;
+  })
+  .post( function() {
+    // If a POST request is made, create the user's profile.
+  })
+  .put( function() {
+    // If a PUT request is made, either update the user's profile or
+   // create it if it doesn't already exist.
+  })
+  .delete( function() {
+   // If a DELETE request is made, delete the user's profile.
+  });
