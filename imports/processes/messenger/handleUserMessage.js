8a663aed546adab5a638095393b91b451491112a
etchemendymat <matteo.etchemendy@supelec.fr>
2017-03-08 19:14:34 +0200

    checking user input

--- imports/processes/messenger/handleUserMessage.js
+++ imports/processes/messenger/handleUserMessage.js
@@ -6,10 +6,22 @@ import { FormGenerators } from '../../api/formgenerators.js';
 
 import { getLastScenario } from '../messenger/getLastScenario.js';
 import { nextStepMessenger } from '../nextScenario.js';
+import { emailSchema , phoneNumberSchema } from '../textValidator/textSchemas.js';
+import { Match } from 'meteor/check';
 
 export function handleUserMessage(discussion, body){
 	lastScenario = getLastScenario(discussion._id);
   messagesPile = discussion.messagesPile;
+
+  mapTextTypeToSchema = {
+    'email' : emailSchema,
+    'phone' : phoneNumberSchema
+  }
+  errorMessages = {
+    'email' : 'Veuillez renseigner un email valide',
+    'phone' : 'Votre numéro de téléphone semble incorrect'
+  }
+  var errorThrown = "";
 
       if (typeof(body.postback) !== 'undefined') {
         idScenario = body.postback.payload;
@@ -50,22 +62,45 @@ export function handleUserMessage(discussion, body){
       }
       else{
         //TEXT INPUT
-
         idScenario = lastScenario.children[0].idScenario;
         idFormGenerator = lastScenario.children[0].idFormGenerator;
         formGenerator = FormGenerators.findOne({'_id' : idFormGenerator});
+        
+        objToCheck = {};  
+
         var userData = {};
-        console.log(formGenerator);
         if (formGenerator.inputType === 'text' && typeof(formGenerator.elements[0].map) === 'undefined') {
           targetName = FormGenerators.findOne({'_id': idFormGenerator}).elements[0].targetName;
           userData[targetName] = body.message.text;
-          Meteor.call("user.update", discussion.idUser, userData);
-          messagesPile.push({
-            'author': 'user',
-            'text': body.message.text,
-            'createdAt': Date(),
-            'idFormGenerator': idFormGenerator 
-          });
+          if (typeof(formGenerator.elements[0].textType) !== 'undefined') {
+            objToCheck[formGenerator.elements[0].textType] = body.message.text;  
+            if (Match.test(objToCheck, mapTextTypeToSchema[formGenerator.elements[0].textType])) {
+              Meteor.call("user.update", discussion.idUser, userData);
+              messagesPile.push({
+                'author': 'user',
+                'text': body.message.text,
+                'createdAt': Date(),
+                'idFormGenerator': idFormGenerator 
+              });
+            } else {
+              idScenario = lastScenario._id;
+              errorThrown = errorMessages[formGenerator.elements[0].textType];
+              messagesPile.push({
+                'author': 'user',
+                'text': body.message.text,
+                'createdAt': Date(), 
+              });
+            }
+          }else{
+            messagesPile.push({
+              'author': 'user',
+              'text': body.message.text,
+              'createdAt': Date(),
+              'idFormGenerator': idFormGenerator 
+            });
+            Meteor.call("user.update", discussion.idUser, userData);
+          }
+
         }else{
           idScenario = lastScenario._id;
         }
@@ -75,5 +110,12 @@ export function handleUserMessage(discussion, body){
 
       //Calling nextStepMessenger with the Id of the scenario we must go to
       data = nextStepMessenger(idScenario, discussion._id);
+      if (errorThrown.length > 0) {  
+        for (var i = data.questions.length - 1; i >= 0; i--) {
+            data.questions[i+1] = data.questions[i]
+        }
+        data.questions[0] = errorThrown;
+      }
+      
       return data;
 }
