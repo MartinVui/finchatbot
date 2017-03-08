e88f455b9db1a287d96a5b8a3b14f8e4dd27a452
etchemendymat <matteo.etchemendy@supelec.fr>
2017-03-08 11:00:53 +0200

    adaptation of messenger handleUserMessage to the new tree collection

--- imports/processes/messenger/getLastScenario.js
+++ imports/processes/messenger/getLastScenario.js
@@ -3,13 +3,15 @@ import { Discussions } from '../../api/discussions.js';
 import { Questions } from '../../api/questions.js';
 import { Users } from '../../api/users.js';
 import { FormGenerators } from '../../api/formgenerators.js';
+import { Trees } from '../../api/trees.js';
 
 
 export function getLastScenario(discussionId){
 	discussion = Discussions.findOne({'_id': discussionId});
+	tree = Trees.findOne({'_id': discussion.idTree});
 	messagesPile = discussion.messagesPile;
 	var userMessages = messagesPile.filter((element)=>{return (typeof(element.idFormGenerator) !== 'undefined')});
-	var cursorScenario = Scenarios.findOne({ '_id' : discussion.idScenario });
+	var cursorScenario = Scenarios.findOne({ '_id' : tree.idInit });
 	for (usermessage of userMessages) {
 		child = cursorScenario.children.filter((element) => {return (element.idFormGenerator === usermessage.idFormGenerator)})[0];
 		if (typeof(child) !== 'undefined') {
