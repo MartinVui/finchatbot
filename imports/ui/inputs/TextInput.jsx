c9e653004703349759259df8490499687d1918ed
etchemendymat <matteo.etchemendy@supelec.fr>
2017-03-08 10:58:14 +0200

    Mail check

--- imports/ui/inputs/TextInput.jsx
+++ imports/ui/inputs/TextInput.jsx
@@ -8,6 +8,9 @@ import { Discussions } from '../../api/discussions.js';
 import { Scenarios } from '../../api/scenarios.js';
 import { FormGenerators } from '../../api/formgenerators.js';
 import { Users } from '../../api/users.js';
+import { Match } from 'meteor/check';
+
+import { emailSchema } from '../../processes/text-validator/emailSchema.js';
 
 import Geosuggest from 'react-geosuggest'
 
@@ -15,58 +18,83 @@ export default class TextInput extends Component {
 
     constructor( props ) {
         super( props );
+        var invalidInputs = {};
         var inputs = {};
+        var errorMessages = {};
         for(element of this.props.formGenerator.elements){
             inputs[element.targetName] = "";
+            invalidInputs[element.targetName] = false;
+            if (element.textType === "email" ) {
+                errorMessages[element.targetName] = "This doesn't look like an email..."
+            }else{
+                errorMessages[element.targetName] = ""
+            }
         }
+        
         this.state = {
-            inputs : inputs
+            inputs : inputs,
+            invalidInputs : invalidInputs,
+            triedToSubmit : false,
+            errorMessages: errorMessages
         };
     }
 
     handleSubmit(event) {
-
         event.preventDefault();
 
-        var text = this.props.formGenerator.generatedAnswer;
+        //Checking if EVERY SINGLE input is valid
+        var invalidData = false;
+        for (formelement in this.state.invalidInputs) {
+            if (this.state.invalidInputs[formelement]) {
+                invalidData = true;
+            }    
+        }
+        //If the user hasn't tried to submit, no reason to display an error
+        //Therefore, after first attempt to submit, we can display an error
+        this.setState({triedToSubmit : true});
+        
+        
+        if (!invalidData) {
+            var text = this.props.formGenerator.generatedAnswer;
 
-        var map = false;
-        for (form of this.props.formGenerator.elements) {
-            if ( "{{"+form.targetName+"}}" === text && form.hasOwnProperty('map') && form.map){
-                map = true;
+            var map = false;
+            for (form of this.props.formGenerator.elements) {
+                if ( "{{"+form.targetName+"}}" === text && form.hasOwnProperty('map') && form.map){
+                    map = true;
+                }
             }
+
+            var answer = Mustache.render(text, this.state.inputs);
+                
+            var formGeneratorId = this.props.formGenerator._id;
+            var discussion = Discussions.findOne({'_id': Session.get('SessionId')});
+            var date = new Date();
+
+                //Update the user
+            var user = Users.findOne({'_id': discussion.idUser});
+            Meteor.call("user.update", user._id, this.state.inputs);
+
+                //Update discussion
+            var messagesPile = Discussions.findOne({
+                '_id' : Session.get( 'SessionId' )
+            }).messagesPile;
+
+            newMessage = {
+                'author' : 'user',
+                'text': answer,
+                'createdAt' : date,
+                'idFormGenerator': formGeneratorId,
+                'map': map
+            }
+
+            messagesPile.push(newMessage);
+            Meteor.call('discussion.update', Session.get("SessionId"), {"messagesPile" : messagesPile});
+                //nextStep Callback here
+            this
+                .props
+                .nextStep( this.props.nextScenario , Session.get("SessionId") );
         }
-
-        var answer = Mustache.render(text, this.state.inputs);
-        // console.log(answer);
-        var formGeneratorId = this.props.formGenerator._id;
-        var discussion = Discussions.findOne({'_id': Session.get('SessionId')});
-        var date = new Date();
-
-        //Update the user
-        var user = Users.findOne({'_id': discussion.idUser});
-        Meteor.call("user.update", user._id, this.state.inputs);
-
-        //Update discussion
-        var messagesPile = Discussions.findOne({
-            '_id' : Session.get( 'SessionId' )
-        }).messagesPile;
-
-        newMessage = {
-            'author' : 'user',
-            'text': answer,
-            'createdAt' : date,
-            'idFormGenerator': formGeneratorId,
-            'map': map
-        }
-
-        messagesPile.push(newMessage);
-        Meteor.call('discussion.update', Session.get("SessionId"), {"messagesPile" : messagesPile});
-
-        //nextStep Callback here
-        this
-            .props
-            .nextStep( this.props.nextScenario , Session.get("SessionId") );
+                
     }
 
 
@@ -94,6 +122,7 @@ export default class TextInput extends Component {
         for ( var i=0;i<this.props.formGenerator.elements.length;i++ ) {
 
             if (this.props.formGenerator.elements[i].map) {
+                
                 outputList.push(
                     <Geosuggest
                         value={this.state.inputs[this.props.formGenerator.elements[i].targetName]}
@@ -101,18 +130,38 @@ export default class TextInput extends Component {
                         targetName='geosuggest'
                         key={i}
                         onChange={this.updateInputValue.bind(this, this.props.formGenerator.elements[i].targetName)}/>)
-                // outputList.push(<input
-                    // value={this.state.inputs[this.props.formGenerator.elements[i].targetName]}
-                    // placeholder={this.props.formGenerator.elements[i].placeholder}
-                    // key={i}
-                    // onChange={this.updateInputValue.bind(this, this.props.formGenerator.elements[i].targetName)}
-                    // id="address-input"/>)
+
             } else {
-                outputList.push(<input
-                    value={this.state.inputs[this.props.formGenerator.elements[i].targetName]}
-                    placeholder={this.props.formGenerator.elements[i].placeholder}
-                    key={i}
-                    onChange={this.updateInputValue.bind(this, this.props.formGenerator.elements[i].targetName)}/>)
+                if (this.state.triedToSubmit) {
+                    console.log("ok");
+                    var inputClassName = "";
+                    var alertMessage = "";
+                    if (this.state.invalidInputs[this.props.formGenerator.elements[i].targetName]) {
+                        alertMessage = <span className='errorAlert'>{this.state.errorMessages[this.props.formGenerator.elements[i].targetName]}</span>
+                        inputClassName = "errorTextInput"
+                    }else{
+                        alertMessage = <span className='successAlert'>{'That\'s better !'}</span>
+                        inputClassName = "successTextInput"
+                    }
+                    outputList.push(
+                        <div className='textInputDiv'>
+                            <input
+                            value={this.state.inputs[this.props.formGenerator.elements[i].targetName]}
+                            placeholder={this.props.formGenerator.elements[i].placeholder}
+                            key={i}
+                            className={inputClassName}
+                            onChange={this.updateInputValue.bind(this, this.props.formGenerator.elements[i].targetName)}/>
+                            {alertMessage}
+                        </div>
+                    )
+                }else{
+                    console.log("cool");
+                    outputList.push(<input
+                        value={this.state.inputs[this.props.formGenerator.elements[i].targetName]}
+                        placeholder={this.props.formGenerator.elements[i].placeholder}
+                        key={i}
+                        onChange={this.updateInputValue.bind(this, this.props.formGenerator.elements[i].targetName)}/>)
+                }
             }
         }
         return (
@@ -124,19 +173,37 @@ export default class TextInput extends Component {
     }
 
     updateInputValue(targetName, evt) {
-        // console.log(targetName);
-        // console.log(evt);
 
-        state = this.state.inputs;
+        var inputState = this.state.inputs;
+        var inputValidations = this.state.invalidInputs;
+
+        // console.log(this.state);
+        
 
         if (evt.hasOwnProperty("target")){
-            state[targetName] = evt.target.value;
+            inputState[targetName] = evt.target.value;
         } else {
-            state[targetName] = evt;
+            inputState[targetName] = evt;
         }
 
+        for (form of this.props.formGenerator.elements) {  
+            if (form.textType === "email") {
+                if(!Match.test({email: inputState[form.targetName]}, emailSchema)){
+                    inputValidations[form.targetName] = true;
+                }else{
+                    inputValidations[form.targetName] = false;
+                } 
+            }else{
+                inputValidations[form.targetName] = false;
+            } 
+        }
+
+        console.log(inputValidations);
+        
+
         this.setState({
-            inputs: state
+            inputs: inputState,
+            invalidInputs: inputValidations
         });
     }
 
