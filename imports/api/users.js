2356f32e358ef46843321311f93a925e658c3a5b
etchemendymat <matteo.etchemendy@supelec.fr>
2017-03-13 16:46:40 +0200

    Voilà

--- imports/api/users.js
+++ imports/api/users.js
@@ -5,27 +5,27 @@ import { UserSchema } from './schemas/userSchema.js';
 
 export const Users = new Mongo.Collection("users");
 
-// if (Meteor.isServer) {
+if (Meteor.isServer) {
 //     // This code only runs on the server
 //     Meteor.publish('users', function usersPublication() {
 //         return Users.find();
 //     });
 // }
-
-Meteor.methods({
-    'user.insert' (user) {
-        check(user, UserSchema);
-        var newUser = Users.insert(user);
-        return newUser;
-    },
-    'user.remove' (userId) {
-        check(userId, String);
-        Users.remove(userId);
-    },
-    'user.update' (userId, update) {
-        check(userId, String);
-        Users.update(userId, {
-            $set: update
-        });
-    },
-});
+    Meteor.methods({
+        'user.insert' (user) {
+            check(user, UserSchema);
+            var newUser = Users.insert(user);
+            return newUser;
+        },
+        'user.remove' (userId) {
+            check(userId, String);
+            Users.remove(userId);
+        },
+        'user.update' (userId, update) {
+            check(userId, String);
+            Users.update(userId, {
+                $set: update
+            });
+        },
+    })
+};
